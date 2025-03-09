51eaeb1ce951ef6e8755883885b1fb5f124444d5
amahadnezhad <amahadnezhad@gmail.com>
2025-03-10 01:20:17 +0330

    Add Poll Class

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.CharField(max_length=700)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+
+
