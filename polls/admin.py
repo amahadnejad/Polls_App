dd4569a58f9992a52a422fbd1078840b1b041794
amahadnezhad <amahadnezhad@gmail.com>
2025-03-10 01:34:52 +0330

    Add User Field To Choice Class

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from .models import Poll, Choice
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ('question', 'user', 'datetime_created')
+    search_fields = ('question',)
+
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('poll', 'user', 'choice_text', 'votes')
+    search_fields = ('choice_text',)
--- polls/models.py
+++ polls/models.py
@@ -8,8 +8,12 @@ class Poll(models.Model):
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.question
+
 
 class Choice(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200, blank=True)
     votes = models.IntegerField(default=0)
