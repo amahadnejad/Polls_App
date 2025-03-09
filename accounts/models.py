e44e3d73b257e80276212a7c124298fe302d291d
amahadnezhad <amahadnezhad@gmail.com>
2025-03-10 01:13:00 +0330

    Add AllAuth Package & Set The Package

--- accounts/models.py
+++ accounts/models.py
@@ -4,4 +4,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Custom User For Auth: You Can Add Custom Fields
 class CustomUser(AbstractUser):
-    job = models.CharField(max_length=300)
+    ROLE_CHOICES = [
+        ('M', 'Manager'),
+        ('E', 'Employee'),
+        ('S', 'Student'),
+        ('O', 'Other'),
+    ]
+    role = models.CharField(choices=ROLE_CHOICES, max_length=1, default=ROLE_CHOICES[3][0])
