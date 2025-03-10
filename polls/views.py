3c7b8bdb359aa1cd039c29096a8358ecaffd713e
amahadnezhad <amahadnezhad@gmail.com>
2025-03-11 01:13:54 +0330

    Create polls_list.html Template

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def poll_list_view(request):
-    return HttpResponse("Hello World")
+    return render(request, 'polls/polls_list.html')
