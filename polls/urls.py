37a336009ff8898f44a6f49f468b81cf6c02ea52
amahadnezhad <amahadnezhad@gmail.com>
2025-03-12 00:41:13 +0330

    Add poll_detail_view

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import poll_list_view
+from .views import poll_list_view, poll_detail_view
 
 urlpatterns = [
     path('', poll_list_view, name='polls_list'),
+    path('poll/<int:pk>/', poll_detail_view, name='poll_detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -10,30 +10,9 @@ def poll_list_view(request):
     })
 
 
+def poll_detail_view(request, pk):
+    poll = Poll.objects.get(pk=pk)
 
-
-
-def poll_vote(request, poll_id):
-    poll = get_object_or_404(Poll, id=poll_id)
-
-    if request.method == 'POST':
-        vote = request.POST.get(f'vote_{poll.id}')
-
-        if vote == 'agree':
-            choice_text = 'Agree'
-        elif vote == 'not_agree':
-            choice_text = 'Not Agree'
-        else:
-            return HttpResponse('Invalid vote', status=400)
-
-        # Create or update the user's vote
-        if Choice.objects.filter(user=request.user, poll=poll).exists():
-            choice = Choice.objects.get(user=request.user, poll=poll)
-            choice.choice_text = choice_text
-            choice.save()
-        else:
-            Choice.objects.create(user=request.user, poll=poll, choice_text=choice_text)
-
-        return redirect('poll_list')  # Redirect back to the poll list page
-
-    return HttpResponse('Invalid method', status=405)
+    return render(request, 'polls/poll_detail.html', context={
+        'poll': poll,
+    })
