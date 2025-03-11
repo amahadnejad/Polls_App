71949805d2bb427e87a72def53d56e92131bd4f7
amahadnezhad <amahadnezhad@gmail.com>
2025-03-12 00:46:09 +0330

    Add Description Field To Poll Model

--- polls/models.py
+++ polls/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth import get_user_model
 class Poll(models.Model):
     question = models.CharField(max_length=700)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
