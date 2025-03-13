a08432449663e54378491ed26de917a1cde6a3dc
amahadnezhad <amahadnezhad@gmail.com>
2025-03-13 15:23:18 +0330

    Add PollForm

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .models import Poll
+from .forms import PollForm
 
 
 def poll_list_view(request):
@@ -14,18 +15,18 @@ def poll_detail_view(request, pk):
     return render(request, 'polls/poll_detail.html', context={'poll': poll})
 
 
+
+
 @login_required
 def up_vote(request, pk):
     poll = Poll.objects.get(pk=pk)
 
     if request.user in poll.downvoted_by.all():
-        # If the user had downvoted, remove downvote and add upvote
         poll.down_vote -= 1
         poll.up_vote += 1
         poll.downvoted_by.remove(request.user)
         poll.upvoted_by.add(request.user)
     elif request.user not in poll.upvoted_by.all():
-        # If the user hasn't voted before or is changing from no vote
         poll.up_vote += 1
         poll.upvoted_by.add(request.user)
 
@@ -38,13 +39,11 @@ def down_vote(request, pk):
     poll = Poll.objects.get(pk=pk)
 
     if request.user in poll.upvoted_by.all():
-        # If the user had upvoted, remove upvote and add downvote
         poll.up_vote -= 1
         poll.down_vote += 1
         poll.upvoted_by.remove(request.user)
         poll.downvoted_by.add(request.user)
     elif request.user not in poll.downvoted_by.all():
-        # If the user hasn't voted before or is changing from no vote
         poll.down_vote += 1
         poll.downvoted_by.add(request.user)
 
