c877e4a6f3cd7e5ee0844e72db496230586d2117
amahadnezhad <amahadnezhad@gmail.com>
2025-03-14 17:21:52 +0330

    Added access control for poll edit/delete, redirect unauthorized users & show notifications

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -145,3 +146,8 @@ ACCOUNT_USERNAME_REQUIRED = True
 
 # Crispy Forms Setting
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# Messages Setting
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages  # Import messages for notifications
+from django.http import HttpResponseForbidden
 
 from .models import Poll
 from .forms import PollForm
@@ -11,8 +13,62 @@ def poll_list_view(request):
 
 
 def poll_detail_view(request, pk):
-    poll = Poll.objects.get(pk=pk)
+    poll = get_object_or_404(Poll, pk=pk)
     return render(request, 'polls/poll_detail.html', context={'poll': poll})
+
+
+@login_required
+def poll_create_view(request):
+    if request.method == 'POST':
+        form = PollForm(request.POST)
+        if form.is_valid():
+            poll = form.save(commit=False)
+            poll.user = request.user
+            poll.save()
+            messages.success(request, "Poll created successfully!")
+            return redirect("polls_list")
+    else:
+        form = PollForm()
+
+    return render(request, 'polls/poll_create.html', {'form': form})
+
+
+@login_required
+def poll_update_view(request, pk):
+    poll = get_object_or_404(Poll, pk=pk)
+
+    # Check if the user is the poll creator
+    if request.user != poll.user:
+        messages.error(request, "You are not allowed to edit this poll.")
+        return redirect("polls_list")  # Redirect to poll list
+
+    if request.method == "POST":
+        form = PollForm(request.POST, instance=poll)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Poll updated successfully!")
+            return redirect("poll_detail", pk=poll.pk)
+    else:
+        form = PollForm(instance=poll)
+
+    return render(request, "polls/poll_update.html", {"form": form, 'poll': poll})
+
+
+@login_required
+def poll_delete_view(request, pk):
+    poll = get_object_or_404(Poll, pk=pk)
+
+    # Check if the user is the poll creator
+    if request.user != poll.user:
+        messages.error(request, "You are not allowed to delete this poll.")
+        return redirect("polls_list")  # Redirect to poll list
+
+    if request.method == "POST":
+        poll.delete()
+        messages.success(request, "Poll deleted successfully!")
+        return redirect("polls_list")
+
+    return render(request, "polls/poll_delete.html", {"poll": poll})
 
 
 @login_required
@@ -33,21 +89,6 @@ def up_vote(request, pk):
 
 
 @login_required
-def poll_create_view(request):
-    if request.method == 'POST':
-        form = PollForm(request.POST)
-        if form.is_valid():
-            poll = form.save(commit=False)
-            poll.user = request.user
-            poll.save()
-            return redirect(poll.get_absolute_url())
-    else:
-        form = PollForm()
-
-    return render(request, 'polls/poll_create.html', {'form': form})
-
-
-@login_required
 def down_vote(request, pk):
     poll = get_object_or_404(Poll, pk=pk)
 
@@ -62,30 +103,3 @@ def down_vote(request, pk):
 
     poll.save()
     return redirect('poll_detail', pk=pk)
-
-
-def poll_update_view(request, pk):
-    poll = get_object_or_404(Poll, pk=pk)
-
-    if request.method == "POST":
-        form = PollForm(request.POST, instance=poll)
-        if form.is_valid():
-            form.save()
-            return redirect("poll_detail", pk=poll.pk)
-    else:
-        form = PollForm(instance=poll)
-
-    return render(request, "polls/poll_update.html", {
-        "form": form,
-        'poll': poll,
-    })
-
-
-def poll_delete_view(request, pk):
-    poll = get_object_or_404(Poll, pk=pk)
-
-    if request.method == "POST":
-        poll.delete()
-        return redirect("polls_list")
-
-    return render(request, "polls/poll_delete.html", {"poll": poll})
